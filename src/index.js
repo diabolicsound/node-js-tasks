9a4516a6128bb582cdc45d44d0697067c500a59f
Artsemi Shareika <ashareika@edmunds.com>
2021-10-27 12:18:27 +0300

    add prettier & limit function

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
-import express, { Router }  from "express";
-import { usersList } from './user.js';
+import express, { Router } from "express";
+import { usersList } from "./user.js";
 
 const app = express();
 const router = Router();
 const port = 10553;
 
-router.get('/', function (req, res) {
+router
+  .get("/", function (req, res) {
     res.json(usersList.showUsers());
-}).post('/', function (req, res) {
+  })
+  .post("/", function (req, res) {
     res.json(usersList.createUser());
+  });
+
+router
+  .get("/:id", function (req, res) {
+    res.json(usersList.getUser(req.params.id));
+  })
+  .delete("/:id", function (req, res) {
+    res.json(usersList.removeUser(req.params.id));
+  });
+
+router.get("/:loginSubstring/:limit", function (req, res) {
+  res.json(usersList.getAutoSuggestUsers(req.params.limit));
 });
 
-router.get('/:id', function (req, res) {
-    res.json(usersList.getUser(req.params.id))
-}).delete('/:id', function (req, res) {
-    res.json(usersList.removeUser(req.params.id))
-});
-
-app.use('/', router)
+app.use("/", router);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
+  console.log(`Server is running at http://localhost:${port}`);
 });
--- src/user.js
+++ src/user.js
@@ -1,52 +1,53 @@
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 const users = [];
 
 class UserClass {
-    constructor() {
-        for (let i = 0; i < 5; i++)
-        {
-            users.push({
-                id: uuidv4(),
-                login: '',
-                password: '',
-                age: 0,
-                isDeleted: false,
-            })
-        }
+  constructor() {
+    for (let i = 0; i < 5; i++) {
+      users.push({
+        id: uuidv4(),
+        login: "",
+        password: "",
+        age: 0,
+        isDeleted: false,
+      });
     }
+  }
 
-    showUsers() {
-        return users;
+  showUsers() {
+    return users;
+  }
+
+  getUser(id) {
+    return users.filter((user) => user.id === id);
+  }
+
+  removeUser(id) {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id === id) {
+        users[i].isDeleted = true;
+        return users[i];
+      }
     }
+  }
 
-    getUser(id) {
-        return users.filter(user => user.id === id);
-    }
+  createUser() {
+    users.push({
+      id: uuidv4(),
+      login: "",
+      password: "",
+      age: 0,
+      isDeleted: false,
+    });
+    return users;
+  }
 
-    removeUser(id) {
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].id === id) {
-                users[i].isDeleted = true;
-                return users[i];
-            }
-        }
-    }
-
-    createUser() {
-        users.push({
-            id: uuidv4(),
-            login: '',
-            password: '',
-            age: 0,
-            isDeleted: false,
-        })
-        return users;
-    }
-
-    getAutoSuggestUsers(loginSubstring, limit) {
-
-    }
+  getAutoSuggestUsers(limit) {
+    users.sort((a, b) => a - b);
+    users.slice(0, limit);
+    return users;
+  }
 }
 
 export const usersList = new UserClass();
